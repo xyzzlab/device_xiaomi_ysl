fcf4ae744a6d3151a3b218feb57651751b69b68c
DhruvChhura <dhruvchhura18@gmail.com>
2023-01-12 17:06:35 +0530

    Revert "ysl: ArrowOS bringup"
    This reverts commit 29dce500d87a53a2df0498028a6c3b0780b66aca.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ysl.mk
+    $(LOCAL_DIR)/aosp_ysl.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ysl-user \
-    arrow_ysl-userdebug \
-    arrow_ysl-eng
+    aosp_ysl-user \
+    aosp_ysl-userdebug \
+    aosp_ysl-eng
